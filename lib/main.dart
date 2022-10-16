dd729790bd16b04e3f44da41812bf587ce6174b7
Reagandennis <dennisowiti215@gmail.com>
2022-10-16 17:48:36 +0300

    margin - edgeinserts

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            color: Colors.white,
+            child: Text('hello there'),
+          ),
+        ),
       ),
     );
   }
