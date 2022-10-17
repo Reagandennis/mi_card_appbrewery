44a35aa2a18ab9b3778719b45aec11a960cb4095
Reagandennis <dennisowiti215@gmail.com>
2022-10-17 08:05:41 +0300

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  get child => null;
+
+  get height => null;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            color: Colors.white,
-            child: Text('hello there'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('lib/images/bike.jpg'),
+              ),
+              Text(
+                'biker mode',
+                style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
           ),
         ),
       ),
