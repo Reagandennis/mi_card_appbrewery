d4211c03255f44efb75ab5876c7bd3d31b835255
Reagandennis <dennisowiti215@gmail.com>
2022-10-19 17:49:12 +0300

    fe

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
