b75c15e1ab1d8be95b1e12b1332b95e119cf4f01
Reagandennis <dennisowiti215@gmail.com>
2022-10-19 16:19:42 +0300

    gdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,18 +26,21 @@ class MyApp extends StatelessWidget {
                 backgroundImage: AssetImage('lib/images/bike.jpg'),
               ),
               Text(
-                'biker mode',
+                'Reagan Enoch',
                 style: TextStyle(
                     fontSize: 35.0,
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
               ),
               Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: Row(
                   children: <Widget>[
                     Icon(
                       Icons.phone,
-                      color: Colors.white,
+                      color: Colors.teal,
                     ),
                     Text(
                       '0796004050',
@@ -44,6 +49,29 @@ class MyApp extends StatelessWidget {
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
                       ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'reagnprezzo@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
                     )
                   ],
                 ),
